43ada53e58dce724fbc1ebb036acffd0e50b50f7
sarrthak008 <sarthaknavale08@gmail.com>
2024-08-26 05:25:04 +0530

    add show page design

--- src/views/Add/Add.js
+++ src/views/Add/Add.js
@@ -7,43 +7,43 @@ import toast, { Toaster } from 'react-hot-toast';
 
 const Add = () => {
 
-  const [tittle , setTittle] = useState('')
-  const [category,setcategory] =useState('ganral')
-  const [description ,setdescription] = useState('')  
-  const [notes , setnotes]  = useState([])
+  const [tittle, setTittle] = useState('')
+  const [category, setcategory] = useState('ganral')
+  const [description, setdescription] = useState('')
+  const [notes, setnotes] = useState([])
 
 
-    // create toast messagegs...
-const notify = (Message,type) =>{
-  if(type==='e'){
-    toast.error(Message)
-  }else if(type === 's'){
-    toast.success(Message)
-  }
-}
- 
-  useEffect(()=>{
-  localStorage.setItem('allNotes',JSON.stringify(notes))
-  notify('your note add suessfully..','s')
-  },[notes])
-
-// set localStorage
-const saveNotes=()=>{
-  setnotes([{tittle,category,description},...notes])
-}
-
-
-//add notes into localStorage....
- const addNotes=()=>{
-    if(tittle.length <=0 && description.length <=0 && category.length==0 ){
-      toast.error('please fill full add fields','e')
-    }else{
-       saveNotes()
-       setTittle('')
-       setcategory('')
-       setdescription('')
+  // create toast messagegs...
+  const notify = (Message, type) => {
+    if (type === 'e') {
+      toast.error(Message)
+    } else if (type === 's') {
+      toast.success(Message)
     }
- }
+  }
+
+  useEffect(() => {
+    localStorage.setItem('allNotes', JSON.stringify(notes))
+    notify('your note add suessfully..', 's')
+  }, [notes])
+
+  // set localStorage
+  const saveNotes = () => {
+    setnotes([{ tittle, category, description }, ...notes])
+  }
+
+
+  //add notes into localStorage....
+  const addNotes = () => {
+    if (tittle.length <= 0 && description.length <= 0 && category === '') {
+      toast.error('please fill full add fields', 'e')
+    } else {
+      saveNotes()
+      setTittle('')
+      setcategory('')
+      setdescription('')
+    }
+  }
 
 
   return (
@@ -52,35 +52,35 @@ const saveNotes=()=>{
       <p className='tag-line '>What you think new , share it !</p>
 
       <div className='form-div'>
-          <input placeholder='enter the tittle'
+        <input placeholder='enter the tittle'
           className='form-inputs'
-           value={tittle} onChange={(e)=>{
-             setTittle(e.target.value)
+          value={tittle} onChange={(e) => {
+            setTittle(e.target.value)
           }}></input>
-          <select placeholder="select the categary" className='form-inputs' onChange={(e)=>{
-             setcategory(e.target.value)
-          }}>
-             <option className='select'>ganral</option>
-             <option className='select'>entarment</option>
-             <option className='select'>study</option>
-             <option className='select'>trip</option>
-             <option className='select'>shopping</option>
-          </select>
-          <textarea className='form-inputs txt-area' placeholder='enter the description' value={description}  onChange={(e)=>{
-             setdescription(e.target.value)
-          }}></textarea>
-         
-          <div className='btns'>
-            <button className='bnt' onClick={()=>{
-              addNotes()
-            }}>Add Note</button>
+        <select placeholder="select the categary" className='form-inputs' onChange={(e) => {
+          setcategory(e.target.value)
+        }}>
+          <option className='select'>ganral</option>
+          <option className='select'>entarment</option>
+          <option className='select'>study</option>
+          <option className='select'>trip</option>
+          <option className='select'>shopping</option>
+        </select>
+        <textarea className='form-inputs txt-area' placeholder='enter the description' value={description} onChange={(e) => {
+          setdescription(e.target.value)
+        }}></textarea>
 
-             <button className='bnt btn-sec' onClick={()=>{
-                setTittle('')
-                setcategory('')
-                setdescription('')
-                notify('clear successfully','s')
-             }}>clear</button>
+        <div className='btns'>
+          <button className='bnt' onClick={() => {
+            addNotes()
+          }}>Add Note</button>
+
+          <button className='bnt btn-sec' onClick={() => {
+            setTittle('')
+            setcategory('')
+            setdescription('')
+            notify('clear successfully', 's')
+          }}>clear</button>
         </div>
       </div>
 
--- src/views/Show/Show.js
+++ src/views/Show/Show.js
@@ -1,9 +1,19 @@
 import React from 'react'
+import 'remixicon/fonts/remixicon.css'
+import './show.css'
 
 const Show = () => {
   return (
-    <div>
-      showw
+    <div className='main-container'>
+      <div className='heading-txt add-heading'>your <span className='text-heighlight'>notes</span></div>
+      <p className='tag-line '>see your all saved notes !</p>
+      <div className='categories'>
+         <span className='category-iteam'>garnal <i class="ri-home-line"></i></span>
+         <span className='category-iteam'>study <i class="ri-book-open-line"></i></span>
+         <span className='category-iteam'>shopping <i class="ri-shopping-bag-line"></i></span>
+         <span className='category-iteam'>entertment <i class="ri-tv-line"></i></span>
+         <span className='category-iteam'>trip <i class="ri-bus-line"></i></span>
+      </div>
     </div>
   )
 }
