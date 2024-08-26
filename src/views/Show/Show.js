45ccf4432451fc289fcd090f2bb709226787d115
sarrthak008 <sarthaknavale08@gmail.com>
2024-08-26 05:33:36 +0530

    resize category option"

--- src/views/Show/Show.js
+++ src/views/Show/Show.js
@@ -8,7 +8,7 @@ const Show = () => {
       <div className='heading-txt add-heading'>your <span className='text-heighlight'>notes</span></div>
       <p className='tag-line '>see your all saved notes !</p>
       <div className='categories'>
-         <span className='category-iteam'>garnal <i class="ri-home-line"></i></span>
+         <span className='category-iteam'>ganral <i class="ri-home-line"></i></span>
          <span className='category-iteam'>study <i class="ri-book-open-line"></i></span>
          <span className='category-iteam'>shopping <i class="ri-shopping-bag-line"></i></span>
          <span className='category-iteam'>entertment <i class="ri-tv-line"></i></span>
